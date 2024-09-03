67e4d498212a9c442b107a947387134e74aafd40
Noziop <contact@fassih.ch>
2024-09-03 17:46:40 +0200

    correcting syntax

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 # Imprimer l'alphabet en minuscules sans nouvelle ligne
-print(''.join(chr(i) for i in range(97, 123)), end='')
+for i in range(97, 123):
+    print("{:c}".format(i), end="")
