512f93525eeb87c119c9be6219e4ac1b3d3c5e40
Noziop <contact@fassih.ch>
2024-09-03 15:16:38 +0200

    FIX : adding guard to execute on 0-add, simplifying code for readability on 1-calc

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-# This module imports functions from calculator_1.py
-# and performs some calculations.
-
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    result_add = add(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = div(a, b)
-
-    print("10 + 5 =", result_add)
-    print("10 - 5 =", result_sub)
-    print("10 * 5 =", result_mul)
-    print("10 / 5 =", result_div)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
