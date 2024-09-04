9eda61e185cb9de09d4a929804ba15b817e650cb
Noziop <contact@fassih.ch>
2024-09-04 10:20:13 +0200

    changing number name not allowed, so changing the last-digit var name

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import random
-num = random.randint(-10000, 10000)
+number = random.randint(-10000, 10000)
 
 # Calculer le dernier chiffre
-if num < 0:
-    last_digit = -(-num % 10)
+if number < 0:
+    l_digit = -(-number % 10)
 else:
-    last_digit = num % 10
+    l_digit = number % 10
 
 # Affichage du résultat selon la valeur du dernier chiffre
-if last_digit > 5:
-    print(f"Last digit of {num} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {num} is {last_digit} and is 0")
+if l_digit > 5:
+    print(f"Last digit of {number} is {l_digit} and is greater than 5")
+elif l_digit == 0:
+    print(f"Last digit of {number} is {l_digit} and is 0")
 else:
-    print(f"Last digit of {num} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
