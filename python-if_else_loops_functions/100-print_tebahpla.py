3bd1db3f22a73bbba9e54e5c5850d51d2ca2bb3e
Noziop <contact@fassih.ch>
2024-09-04 12:07:23 +0200

    FIX : using .format() is mandatory yet not mentionned

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-print(''.join([chr(i + 32) if i % 2 == 0 else chr(i) for i in range(90, 64, -1)]))
+print(''.join(['{:c}'.format(i) if i % 2 == 0 else '{:c}'.format(i - 32) for i in range(122, 96, -1)]))
