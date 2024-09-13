693410e6bc041b0b6079ef32670fdf47ae30f660
Noziop <contact@fassih.ch>
2024-09-13 11:34:22 +0200

    pycode

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a function that prints a text with 2 new lines after each of these characters: ., ? and :
+This module contains a function that prints a text
+with 2 new lines after each of these characters: ., ? and :
 """
 
 
