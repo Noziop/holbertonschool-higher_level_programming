40347bda6134127a4f3bc0d9629ebbef9126a809
Noziop <contact@fassih.ch>
2024-09-17 10:53:14 +0200

    correction pycode and matching the assignement output

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -32,3 +32,39 @@ class Node:
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
+
+class SinglyLinkedList:
+    '''A singly linked list class'''
+    def __init__(self):
+        '''Initialization of the linked list'''
+        self.__head = None
+
+    def sorted_insert(self, value):
+        '''Insert a new Node into the correct sorted position'''
+        new_node = Node(value)
+
+        if self.__head is None:
+            self.__head = new_node
+            return
+
+        if value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+
+        current = self.__head
+        while current.next_node and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def __str__(self):
+        '''String representation of the linked list'''
+        values = []
+        current = self.__head
+        while current:
+            values.append(str(current.data))
+            current = current.next_node
+        return '\n'.join(values)
--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -4,13 +4,10 @@
 
 class Square:
     '''A simple Square class'''
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         '''Initialization of instance attributes'''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
+        self.position = position
 
     def area(self):
         '''Calculates the area'''
@@ -18,38 +15,50 @@ class Square:
 
     @property
     def size(self):
-        '''Getter for size'''
+        '''Getter method for size'''
         return self.__size
 
     @size.setter
     def size(self, value):
-        '''Setter for size'''
+        '''Setter method for size'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    def __lt__(self, other):
-        '''Less than'''
-        return self.area() < other.area()
-    def __le__(self, other):
-        '''Less than or equal'''
-        return self.area() <= other.area()
-    def __eq__(self, other):
-        '''Equal to'''
-        return self.area() == other.area()
-    def __ne__(self, other):
-        '''Not equal to'''
-        return self.area() != other.area()
-    def __gt__(self, other):
-        '''Greater than'''
-        return self.area() > other.area()
-    def __ge__(self, other):
-        '''Greater than or equal'''
-        return self.area() >= other.area()
+
+    @property
+    def position(self):
+        '''Getter method for position'''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        '''Setter method for position'''
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        '''Prints the square'''
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
     def __str__(self):
-        '''String representation of the square'''
-        return "#" * self.__size
-    def __repr__(self):
-        '''String representation of the square'''
-        return "Square({})".format(self.__size)
+        '''Returns a string representation of the square'''
+        if self.__size == 0:
+            return ""
+
+        result = []
+        for _ in range(self.__position[1]):
+            result.append("")
+        for _ in range(self.__size):
+            result.append(" " * self.__position[0] + "#" * self.__size)
+        return "\n".join(result)
