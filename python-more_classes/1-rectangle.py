b4fe8b4ca44d30f469a17bb670fb904d2515d1c6
Noziop <contact@fassih.ch>
2024-09-17 13:45:36 +0200

    time to push all exercices to see if i got it right

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Module for Rectangle class'''
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        '''Initialization of instance attributes'''
+        self.width = width
+        self.height = height
+
+    def width(self):
+        '''Getter method for width attribute'''
+        return self.__width
+    
+    def width(self, value):
+        '''Setter method for width attribute'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    def height(self):
+        '''Getter method for height attribute'''
+        return self.__height
+    
+    def height(self, value):
+        '''Setter method for height attribute'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''Module for Rectangle class'''
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        '''Initialization of instance attributes'''
+        self.width = width
+        self.height = height
+
+    def width(self):
+        '''Getter method for width attribute'''
+        return self.__width
+    
+    def width(self, value):
+        '''Setter method for width attribute'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    def height(self):
+        '''Getter method for height attribute'''
+        return self.__height
+    
+    def height(self, value):
+        '''Setter method for height attribute'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        '''Return the area of the rectangle'''
+        return self.width * self.height
+
+    def perimeter(self):
+        '''Return the perimeter of the rectangle'''
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
