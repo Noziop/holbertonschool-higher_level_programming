dedcbe1148e210b9146f3ac2296549c0e1a3fb8f
Noziop <contact@fassih.ch>
2024-09-17 10:37:15 +0200

    why would i test all my files once and for all, when i can test my patience and test them one at the time, huh ?

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -29,21 +29,27 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def __lt__(self, other):
         '''Less than'''
         return self.area() < other.area()
+
     def __le__(self, other):
         '''Less than or equal'''
         return self.area() <= other.area()
+
     def __eq__(self, other):
         '''Equal to'''
         return self.area() == other.area()
+
     def __ne__(self, other):
         '''Not equal to'''
         return self.area() != other.area()
+
     def __gt__(self, other):
         '''Greater than'''
         return self.area() > other.area()
+
     def __ge__(self, other):
         '''Greater than or equal'''
         return self.area() >= other.area()
