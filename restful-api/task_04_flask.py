9cb69b2407cd6c67098e0cb4c1561fe58765b4cd
Noziop <contact@fassih.ch>
2024-10-10 13:34:40 +0200

    trying some stuff

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python3
 ''' Module that implements a simple API '''
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-
-users = {}
-
+users = {
+    "jane": {
+        "username": "jane",
+        "name": "Jane",
+        "age": 28,
+        "city": "Los Angeles"},
+    "john": {
+        "username": "john",
+        "name": "John",
+        "age": 30,
+        "city": "New York"}
+}
 
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
 
+@app.route('/clear_users')
+def clear_users():
+    global users
+    users = {}
+    return "Users cleared"
 
 @app.route('/data')
 def data():
-    return jsonify([user['username'] for user in users.values()])
-
+    if users == {}:
+        abort(404)
+    return jsonify(list(users.keys()))
 
 @app.route('/status')
 def status():
     return "OK"
-
 
 @app.route('/users/<username>')
 def get_user(username):
@@ -32,21 +48,19 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    new_user = request.form.to_dict() if request.form else request.json
+    new_user = request.get_json(force=True)
     if not new_user or 'username' not in new_user:
         return jsonify({"error": "Username is required"}), 400
     username = new_user['username']
-    if username in users:
+    if username and 'name' and 'age' and 'city' in users:
         return jsonify({"error": "Username already exists"}), 400
     users[username] = new_user
     return jsonify({
         "message": "User added",
         "user": new_user
     }), 201
-
 
 if __name__ == '__main__':
     app.run(debug=True)
