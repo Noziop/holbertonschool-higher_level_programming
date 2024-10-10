25473c6ef1620573f5d1d2de9c1b9fadc66b5f0a
Noziop <contact@fassih.ch>
2024-10-10 18:18:37 +0200

    pycodestyle fixing

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -9,9 +9,11 @@ CORS(app)
 
 users = {}
 
+
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route('/clear_users')
 def clear_users():
@@ -19,13 +21,16 @@ def clear_users():
     users.clear()
     return jsonify({"message": "All users cleared"}), 200
 
+
 @app.route('/data')
 def data():
     return jsonify(list(users.keys()))
 
+
 @app.route('/status')
 def status():
     return "OK"
+
 
 @app.route('/users/<username>')
 def get_user(username):
@@ -35,21 +40,23 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
+
 @app.route('/add_user', methods=['POST'])
 def add_user():
     new_user = request.get_json(force=True)
     if not new_user or 'username' not in new_user:
         return jsonify({"error": "Username is required"}), 400
-    
+
     username = new_user['username']
     if username in users:
         return jsonify({"error": "Username already exists"}), 400
-    
+
     users[username] = new_user
     return jsonify({
         "message": "User added",
         "user": new_user
     }), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
