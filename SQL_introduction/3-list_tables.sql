e904a519e942c1166139ce86509794f2310d7996
Noziop <contact@fassih.ch>
2024-10-29 16:23:18 +0100

    FIX : have been a little bit too much careful by recreating the DB :D

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
--- As we drop the database hbtn_0c_0, we need to recreate it
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- then list all tables of the database
-USE hbtn_0c_0;
+-- List all tables of a database
 SHOW TABLES;
