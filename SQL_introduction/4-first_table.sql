f9fcd539df508de2539b9a059f321f8186ae1a36
Noziop <contact@fassih.ch>
2024-10-29 16:28:56 +0100

    FIX : tidying up the script, removing extra chars

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,6 @@
 -- Create table first_table
--- choose the db first
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
--- create the table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
