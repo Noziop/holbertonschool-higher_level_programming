bad0bc8922ff7094d4a8b81b5adc013faf167e83
Noziop <contact@fassih.ch>
2024-10-30 11:15:24 +0100

    FIX : using format on query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,10 +16,9 @@ if __name__ == "__main__":
     # Create cursor to execute queries
     cursor = db.cursor()
 
-    # Execute query with user input (ATTENTION: SQL Injection possible!)
-    cursor.execute("SELECT * FROM states \
-                   WHERE name = '{}' \
-                   ORDER BY id ASC".format(sys.argv[4]))
+    # Execute query with format (as required)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cursor.execute(query.format(sys.argv[4]))
 
     # Fetch and print results
     states = cursor.fetchall()
